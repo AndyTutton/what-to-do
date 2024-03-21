c48744fcb1b3d0b3b4e7c348b3dd0428d8d50fd2
Andy Tutton <andytutton2@gmail.com>
2024-03-21 15:25:38 +0000

    updating

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,7 +3,7 @@ class Activity < ApplicationRecord
   has_many :saves
   has_many_attached :photos
   geocoded_by :address
- #test tes test
+
   after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
 
 end
